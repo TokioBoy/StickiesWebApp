3d0459228c85d0f13e9ca19cbb362068f659628e
Vlad <70889664+TokioBoy@users.noreply.github.com>
2022-07-06 12:09:18 +0200

    upd

---
+++ app.js
@@ -0,0 +1,8 @@
+    
+        let year = ['spring', 'summer', 'autumn', 'winter'];
+
+
+        year.forEach((item, index ) =>{
+        //  перечисляет элементы массива
+            console.log(item, index);
+        })
